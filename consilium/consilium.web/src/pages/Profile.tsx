de03275b68714914957d4b1e22592338281bffe6
MateoTomaszeuski <mtomaszeuski@gmail.com>
2026-01-27 20:38:03 -0700

    linting

--- consilium/consilium.web/src/pages/Profile.tsx
+++ consilium/consilium.web/src/pages/Profile.tsx
@@ -89,8 +89,6 @@ export const Profile = () => {
     );
   }
 
-  }
-
   return (
     <div className="max-w-4xl mx-auto">
       <h1 className="text-3xl font-bold mb-8 text-dark-dark text-center">Profile</h1>
