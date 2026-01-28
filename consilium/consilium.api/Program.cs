92a8aa5270d09ac87a69ec7200427273a8654ceb
MateoTomaszeuski <mtomaszeuski@gmail.com>
2026-01-27 17:48:20 -0700

    format

--- consilium/consilium.api/Program.cs
+++ consilium/consilium.api/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy.WithOrigins(
-                "http://localhost:5173", 
+                "http://localhost:5173",
                 "https://localhost:5173",
                 "https://consilium.mateo.tomaszeuski.com")
               .AllowAnyHeader()
